f73f6e2ec1feee8197d60fb05c70b3804c1f9473
Nick Comito <nick@astonishdesign.com>
2014-11-10 13:50:14 -0600

    fix masonry

--- js/app.js
+++ js/app.js
@@ -11,3 +11,12 @@ var $container = $('#brick-house').masonry({
 $container.imagesLoaded( function() {
   $container.masonry();
 });
+
+// $(document).ready({
+// 	var container = document.querySelector('#brick-house');
+// 	var msnry = new Masonry( container, {
+// 	  // options
+// 	  columnWidth: 150,
+// 	  itemSelector: '.brick'
+// 	});
+// });
